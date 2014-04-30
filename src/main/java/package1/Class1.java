8d3c8d1a43adff0cc3ad0a83e126d67e7a8e484c
ngandriau <nicolas.gandriau@hybris.com>
2014-04-30 11:36:24 -0400

    method4

--- src/main/java/package1/Class1.java
+++ src/main/java/package1/Class1.java
@@ -5,6 +5,9 @@ public class Class1
 	public void method2(){
 		System.out.println("method2");
 	}
+	public void method4(){
+		System.out.println("method4");
+	}
 	public void method1(){
 		System.out.println("method1");
 	}
