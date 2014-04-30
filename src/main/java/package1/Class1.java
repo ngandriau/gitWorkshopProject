2c2c01d4701201d9a84078386a45a5d24db37902
ngandriau <nicolas.gandriau@hybris.com>
2014-04-30 11:32:04 -0400

    first class

--- src/main/java/package1/Class1.java
+++ src/main/java/package1/Class1.java
@@ -5,6 +5,10 @@ public class Class1
 	public void method2(){
 		System.out.println("method2");
 	}
+	public void method3(){
+		System.out.println("method3");
+	}
+
 	public void method1(){
 		System.out.println("method1");
 	}
